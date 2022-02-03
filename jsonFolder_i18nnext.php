e2da207ce3c514f5cd660a5bb456dbaf458f1830
John R. D'Orazio <priest@johnromanodorazio.com>
2022-02-03 15:42:21 +0100

    update source translation strings

--- jsonFolder_i18nnext.php
+++ jsonFolder_i18nnext.php
@@ -1,16 +1,15 @@
 <?php
 
-$srcDir     = './en';
-$destDir    = './it';
+$dir    = './it';
 
-$files1 = array_diff(scandir($srcDir), ['..','.','translation.json']);
+$files1 = array_diff(scandir($dir), ['..','.','translation.json']);
 
 $translationJson = new stdClass();
 
 foreach( $files1 as $file ) {
     $name = explode( '.', $file )[0];
     $translationJson->{$name} = new stdClass();
-    $jsonArray = json_decode( file_get_contents( "$srcDir/$file" ), true );
+    $jsonArray = json_decode( file_get_contents( "$dir/$file" ), true );
     $currentStoryId = "";
     foreach( $jsonArray as $entry ) {
         if( $entry["storyId"] !== $currentStoryId ) {
@@ -24,7 +23,7 @@ foreach( $files1 as $file ) {
 }
 
 //print_r( json_encode( $translationJson ) );
-if( false !== file_put_contents( "{$destDir}/translation.json", json_encode( $translationJson, JSON_PRETTY_PRINT ) ) ) {
+if( false !== file_put_contents( "{$dir}/translation.json", json_encode( $translationJson, JSON_PRETTY_PRINT ) ) ) {
     echo "Successfully saved JSON to file it/translation.json" . PHP_EOL;
 };
 die();
